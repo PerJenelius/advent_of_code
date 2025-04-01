fb9ecf9f5359aab943ae4e60d752a19c7499f885
PerJenelius <per.jenelius@gmail.com>
2025-04-01 16:53:16 +0200

    Working solution to Dec 5

---
+++ 2024/dec_05_a/app.py
@@ -0,0 +1,85 @@
+import time
+
+
+def get_indata(file_name: str):
+    with open(file_name, "r") as file:
+        content = file.readlines()
+        return content
+
+
+def get_rules(data: str):
+    rules = []
+    for line_index in range(len(data)):
+        line = data[line_index]
+        if "|" in line:
+            rules.append({"left": line.split("|")[0], "right": line.split("|")[1].rstrip()})
+    return rules
+
+
+def get_updates(data: str):
+    updates = []
+    for line_index in range(len(data)):
+        line = data[line_index]
+        if "," in line:
+            update = []
+            for page in line.split(","):
+                update.append(page.rstrip())
+            updates.append(update)
+    return updates
+
+
+def find_median_value(values: list):
+    index = len(values) // 2
+    return int(values[index])
+
+
+def find_correct_updates(data: str):
+    sum_medians = 0
+    rules = get_rules(data)
+    updates = get_updates(data)
+    for update in updates:
+        correct = True
+        for page_index in range(len(update)):
+            page = update[page_index]
+            for rule in rules:
+                if page == rule["left"]:
+                    for other_page_index in range(len(update)):
+                        if other_page_index == page_index:
+                            continue
+                        other_page = update[other_page_index]
+                        if other_page == rule["right"] and other_page_index < page_index:
+                            correct = False
+                elif page == rule["right"]:
+                    for other_page_index in range(len(update)):
+                        if other_page_index == page_index:
+                            continue
+                        other_page = update[other_page_index]
+                        if other_page == rule["left"] and other_page_index > page_index:
+                            correct = False
+        if correct:
+            sum_medians += find_median_value(update)
+    return sum_medians
+
+
+def main():
+    file_definitions = ("test_data.txt", "indata.txt")
+    expected_test_result = 143
+
+    start_time = time.time()
+    print(f"Calculating...\n")
+
+    test_data = get_indata(file_definitions[0])
+    test_discrepancy = find_correct_updates(test_data)
+    print("Test Result:", test_discrepancy)
+    print("-Expected--:", expected_test_result)
+
+    data = get_indata(file_definitions[1])
+    discrepancy = find_correct_updates(data)
+    print("Real Result:", discrepancy)
+
+    stop_time = time.time()
+    elapsed_time = round(stop_time - start_time, 3)
+    print(f"\nElapsed time: {elapsed_time}s")
+
+
+main()
