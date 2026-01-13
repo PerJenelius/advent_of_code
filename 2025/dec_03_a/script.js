3cbce3e86f4e4fad0dd4718ce53df197170d9986
Per Jenelius <per.jenelius@gmail.com>
2026-01-13 10:50:06 +0100

    Solution for 2025-12-03 A

---
+++ 2025/dec_03_a/script.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const app = {
+    startPosition: 50,
+    testAnswer: 0,
+    realAnswer: 0,
+};
+
+const getMaximumJoltage = (indata) => {
+    const banks = indata.split('\n')
+        .map(line => line.trim())
+        .filter(line => line.length > 0);
+    let maximumJoltage = 0;
+    for (let bank of banks) {
+        let firstMaxIndex = 0;
+        for (let i = 0; i < (bank.length - 1); i++) {
+            if (parseInt(bank[i]) > parseInt(bank[firstMaxIndex])) {
+                firstMaxIndex = i;
+            }
+        }
+        let secondMaxIndex = firstMaxIndex + 1;
+        for (let j = (firstMaxIndex + 1); j < bank.length; j++) {
+            if (parseInt(bank[j]) > parseInt(bank[secondMaxIndex])) {
+                secondMaxIndex = j;
+            }
+        }
+        maximumJoltage += parseInt(`${bank[firstMaxIndex]}${bank[secondMaxIndex]}`);
+    }
+    return maximumJoltage;
+}
+
+const updateTemplate = () => {
+    document.querySelector('#test-answer').innerHTML = app.testAnswer;
+    document.querySelector('#real-answer').innerHTML = app.realAnswer;
+}
+
+const main = () => {
+    app.testAnswer = getMaximumJoltage(testData());
+    app.realAnswer = getMaximumJoltage(realData());
+    updateTemplate();
+}
+
+if (document.readyState === 'complete' || (document.readyState !== 'loading' && !document.documentElement.doScroll)) {
+    main();
+} else {
+    document.addEventListener('DOMContentLoaded', main);
+}
--- README.md
+++ README.md
@@ -8,8 +8,8 @@ Current status of the project:<br/>
 		<td>01</td><td>02</td><td>03</td><td>04</td><td>05</td><td>06</td><td>07</td><td>08</td><td>09</td><td>10</td><td>11</td><td>12</td><td>13</td><td>14</td><td>15</td><td>16</td><td>17</td><td>18</td><td>19</td><td>20</td><td>21</td><td>22</td><td>23</td><td>24</td><td>25</td><td>Sum&nbsp;per&nbsp;year</td>
 	</tr>
 	<tr>
-		<td><strong>2025 (Javascript)</strong></td>
-		<td>**</td><td>**</td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td>--</td><td>--</td><td>--</td><td>--</td><td>--</td><td>--</td><td>--</td><td>--</td><td>--</td><td>--</td><td>--</td><td>--</td><td>--</td><td>4 / 24</td>
+		<td><strong>2025 (JavaScript)</strong></td>
+		<td>**</td><td>**</td><td>*</td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td>--</td><td>--</td><td>--</td><td>--</td><td>--</td><td>--</td><td>--</td><td>--</td><td>--</td><td>--</td><td>--</td><td>--</td><td>--</td><td>5 / 24</td>
 	</tr>
 	<tr>
 		<td><strong>2024 (Python)</strong></td>
@@ -52,4 +52,4 @@ Current status of the project:<br/>
 		<td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td>0 / 50</td>
 	</tr>
 </table>
-Total: 23 / 524 stars
+Total: 24 / 524 stars
